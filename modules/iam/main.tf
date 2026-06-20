a94f6071c1f0f2a3d951db7e629e232ce5314131
Timothy Olubiyi <86630962+Timothyolubiyi@users.noreply.github.com>
2026-06-20 17:32:02 +0100

    new update

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -11,34 +11,57 @@ data "aws_caller_identity" "current" {}
 data "aws_iam_policy_document" "github_assume_role" {
 
   statement {
+
     effect = "Allow"
 
+    actions = [
+      "sts:AssumeRoleWithWebIdentity"
+    ]
+
     principals {
-      type        = "Federated"
+
+      type = "Federated"
+
       identifiers = [
-        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:oidc-provider/token.actions.githubusercontent.com"
+        aws_iam_openid_connect_provider.github.arn
       ]
     }
 
-    actions = ["sts:AssumeRoleWithWebIdentity"]
-
     condition {
-      test     = "StringEquals"
+
+      test = "StringEquals"
+
       variable = "token.actions.githubusercontent.com:aud"
-      values   = ["sts.amazonaws.com"]
+
+      values = [
+        "sts.amazonaws.com"
+      ]
     }
 
     condition {
-      test     = "StringLike"
-      variable = "token.actions.githubusercontent.com:sub:repo:https://github.com/Timothyolubiyi/Enterprise-Automated-Application-Security-Policy-Enforcement-Pipeline.git:*"
-      values   = ["repo:*:*"]
+
+      test = "StringLike"
+
+      variable = "token.actions.githubusercontent.com:sub"
+
+      values = [
+        "repo:Timothyolubiyi/Enterprise-Automated-Application-Security-Policy-Enforcement-Pipeline:ref:refs/heads/main"
+      ]
     }
   }
 }
 
-resource "aws_iam_role" "github_actions" {
-  name               = "GitHubActionsOIDCRole"
-  assume_role_policy = data.aws_iam_policy_document.github_assume_role.json
+resource "aws_iam_openid_connect_provider" "github" {
+
+  url = "https://token.actions.githubusercontent.com"
+
+  client_id_list = [
+    "sts.amazonaws.com"
+  ]
+
+  thumbprint_list = [
+    "6938fd4d98bab03faadb97b34396831e3780aea1"
+  ]
 }
 
 ############################################
